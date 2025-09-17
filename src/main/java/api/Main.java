2d17cb6c4b039a4d97d17b7db100f6cb35798c6c
Isabsk <isabsk10@gmail.com>
2025-09-17 16:49:03 +0530

    App to Main

--- src/main/java/api/Main.java
+++ src/main/java/api/Main.java
@@ -7,7 +7,7 @@ import io.javalin.http.Context;
 
 public class Main {
 
-    private static final String API_KEY = System.getenv("API_KEY");
+    private static final String API_KEY = System.getenv("LOTTO_API_KEY");
 
     public static void main(String[] args) {
         System.out.println(System.getenv("API_KEY"));
@@ -25,9 +25,20 @@ public class Main {
             ctx.status(204);
         });
 
+        app.get("/", ctx -> ctx.json("Lotto Checker API is running."));
+
         app.get("/{time}/{series}/{number}", Check::getData);
 
-        app.start(7070);
+        String portEnv = System.getenv("X_ZOHO_CATALYST_LISTEN_PORT");
+        int port;
+
+        if (portEnv != null && !portEnv.isEmpty()) {
+            port = Integer.parseInt(portEnv);
+        } else {
+            port = 7070;
+        }
+
+        app.start(port);
     }
 
     private static void checkApiKey(Context ctx) {
